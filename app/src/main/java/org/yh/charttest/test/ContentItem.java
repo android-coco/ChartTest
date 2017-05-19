3e6660ff4e839b5a98aae108ab5d468731f0e6a6
yhlyl <yh_android@163.com>
2017-05-19 22:28:01 +0800

    LineChart

---
+++ app/src/main/java/org/yh/charttest/test/ContentItem.java
@@ -0,0 +1,18 @@
+package org.yh.charttest.test;
+
+/**
+ * Created by Philipp Jahoda on 07/12/15.
+ */
+public class ContentItem
+{
+
+    String name;
+    String desc;
+    boolean isNew = false;
+
+    public ContentItem(String n, String d)
+    {
+        name = n;
+        desc = d;
+    }
+}
